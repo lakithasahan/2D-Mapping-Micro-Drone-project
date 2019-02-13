0d1fb9da6671569f6a51c77e59ecc00ac61374bf
lakithasahan <lakithasahanhettiarachchi@gmail.com>
2019-02-14 10:44:24 +1100

    Add files via upload

---
+++ Arduino_Mapping_Demo/ReadMe.txt
@@ -0,0 +1,18 @@
+Mapping Your Surroundings Using MATLAB and Arduino
+------------------------------------------------
+Description:
+This project utilizes an Arduino board, a servo motor and an ultrasonic sensor to gather the data 
+necessary to construct a "map" of the environment via MATLAB. 
+
+To Run This Demo:
+Step 1: Construct the circuit shown in the provided schematic
+Step 2: Run the provided MATLAB script 
+
+Notes:
+1) The circuit schematic for the project has been included as "Arduino_Mapping_Schematic.png" along with the
+MATLAB file for this demo. 
+
+2) Low-cost ultrasonic sensors can be noisy so it may impact the performance of this project. A capacitor can be added between 
+the power and ground pins of the sensor to help mitigate these undesirable results, however, it is not required for the project 
+to function and does not guarantee performance enhancement. 
+
---
+++ lidararduinocode/lidararduinocode.ino
@@ -0,0 +1,58 @@
+// Ultrasonic - Library for HR-SC04 Ultrasonic Ranging Module.
+// Rev.4 (06/2012)
+// J.Rodrigo ( www.jrodrigo.net )
+// more info at www.ardublog.com
+
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+#include <Servo.h>
+
+Servo myservo;
+int i=0;
+void setup() {
+  Serial.begin(9600); 
+  myservo.attach(3); 
+  myservo.write(0);
+   if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+}
+
+void loop()
+{
+ VL53L0X_RangingMeasurementData_t measure;
+
+if(Serial.available()>0){
+  char a=Serial.read();
+  
+  if(a =='a'){
+   myservo.write(i);
+   delay(15);
+   lox.rangingTest(&measure, false);
+   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+     Serial.println((measure.RangeMilliMeter/10));
+  } else {
+    Serial.println(0);
+  }
+   i=i+1; 
+  }
+  else if(a =='b'){
+     myservo.write(i);
+     delay(15);
+     lox.rangingTest(&measure, false);
+     //delay(10);
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+     Serial.println((measure.RangeMilliMeter/10));
+  } else {
+    Serial.println(0);
+  }
+   i=i-1; 
+  }
+  
+}
+  
+
+  
+}
